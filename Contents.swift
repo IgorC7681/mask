397001d28aa25bd036630da2c9f282eee7333044
TaiYIChien <boss760801@gmail.com>
2020-03-17 14:00:24 +0800

    iOS mask

---
+++ Contents.swift
@@ -0,0 +1,38 @@
+//import UIKit
+//let auroraImage = UIImage(named: "aurora")
+//let auroraImageView = UIImageView(image: auroraImage)
+//let coupleImage = UIImage(named: "couple")
+//let coupleImageView = UIImageView(image: coupleImage)
+//auroraImageView.mask = coupleImageView
+
+
+
+//import UIKit
+//let auroraImage = UIImage(named: "aurora")
+//let auroraImageView = UIImageView(image: auroraImage)
+//let coupleImage = UIImage(named: "couple")
+//let coupleImageView1 = UIImageView(image: coupleImage)
+//auroraImageView.mask = coupleImageView1
+//let redView = UIView(frame: auroraImageView.frame)
+//redView.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.5)
+//let coupleImageView2 = UIImageView(image: coupleImage)
+//redView.mask = coupleImageView2
+//auroraImageView.addSubview(redView)
+
+
+
+//import UIKit
+//let auroraImage = UIImage(named: "aurora")
+//let auroraImageView = UIImageView(image: auroraImage)
+//let coupleImage = UIImage(named: "couple")
+//let coupleImageView1 = UIImageView(image: coupleImage)
+//auroraImageView.mask = coupleImageView1
+//auroraImageView.alpha = 0.5
+//let loveImage = UIImage(named: "love")
+//let loveImageView = UIImageView(image: loveImage)
+//loveImageView.alpha = 0.5
+//let loveImageView2 = UIImageView(image: coupleImage)
+//loveImageView.mask = loveImageView2
+//let backgroundView = UIView(frame: auroraImageView.frame)
+//backgroundView.addSubview(auroraImageView)
+//backgroundView.addSubview(loveImageView)
